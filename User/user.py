cc6217761cf4cab45965f5d083b62baf3098fe4a
BihawaM <bihawam271@gmail.com>
2020-10-11 18:52:52 +0300

    add class User

---
+++ User/user.py
@@ -0,0 +1,81 @@
+# import pyperclip
+
+class User:
+    """
+    Class that generates new instances of users.
+    """
+    
+    user_list = []
+    
+    
+    # def save_user(self):
+    #     """
+    #     save_user method saves user objects into user_list
+    #     """
+        
+    #     User.user_list.append(self)
+        
+        
+    # def __init__(self,details,login_username,password):
+        
+    #     self.details = details
+    #     self.login_username = login_username
+    #     self.password = password
+        
+    
+    # def delete_user(self):
+    #     """
+    #     delete_user method deletes a saved user from the user_list
+    #     """
+        
+    #     User.user_list.remove(self)
+        
+        
+    # @classmethod
+    # def find_by_details(cls,details):
+    #     """
+    #     Method that takes in a details and returns a user that matches the details.
+        
+    #     Args: 
+    #         details: details to search for
+    #     Returns:
+    #         Details of user that matches the details.     
+    #     """
+        
+    #     for user in cls.user_list:
+    #         if user.details == details:
+    #             return user
+        
+        
+    # @classmethod
+    # def user_exists(cls,details):
+    #     """
+    #     Method that checks if a user existsfrom the user list.
+    #     Args:
+    #         details: details to search if it exists
+    #     Returns:
+    #         Boolean: True or false depensing if the user exists.     
+    #     """
+        
+    #     for user in cls.user_list:
+    #         if user.details == details:
+    #             return True
+            
+    #     return False
+    
+    
+    # @classmethod
+    # def display_users(cls):
+    #     """
+    #     returns the user list
+    #     """
+        
+    #     return cls.user_list
+    
+    
+    
+    # @classmethod
+    # def copy_password(cls,details):
+    #     user_found = User.find_by_details(details)
+    #     pyperclip.copy(user_found.password)    
+                
---
+++ User/user_test.py
@@ -0,0 +1,118 @@
+# import pyperclip
+# import unittest
+# from user import User 
+
+
+# class TestUser(unittest.TestCase):
+#     """
+#     Test class that defines test cases for the user class behaviours.
+#     """
+    
+    
+#     def setUp(self):
+#         """
+#         Set up method to run before each test case
+#         """
+        
+#         self.new_user = User("Instagram password","Bello_Murray","pass1234#")
+        
+    
+#     def test_init(self):
+#         """
+#         test_init test case to test if the object is initialized properly
+#         """
+        
+#         self.assertEqual(self.new_user.details,"Instagram password")
+#         self.assertEqual(self.new_user.login_username,"Bello_Murray")
+#         self.assertEqual(self.new_user.password,"pass1234#")
+        
+        
+#     def test_save_user(self):
+#         """
+#         test_save_user test case to test if the user object is saved into the user list
+#         """
+        
+#         self.new_user.save_user()
+#         self.assertEqual(len(User.user_list),1)     
+        
+        
+#     def tearDown(self):
+#         """
+#         tearDown method that does clean up after each test case has run
+#         """
+        
+#         User.user_list = []
+        
+     
+#     def test_save_multiple_user(self):
+#         """  
+#         test_save_multiple_user checks if we can save multiple user objects to our user_list
+#         """
+        
+#         self.new_user.save_user()
+#         test_user = User("Facebook password","Bihawa Mohamed","65432*")
+#         test_user.save_user()
+#         self.assertEqual(len(User.user_list),2)
+        
+        
+#     def test_delete_user(self):
+#         """
+#         test_delete_user tests if we can remove a contact from our contact list
+#         """
+        
+#         self.new_user.save_user()
+#         test_user = User("Facebook","Bihawa Mohamed","65432*")
+#         test_user.save_user()
+        
+#         self.new_user.delete_user()
+#         self.assertEqual(len(User.user_list),1)
+        
+        
+#     def test_find_user_by_details(self):
+#         """
+#         test to check if we can find a user by details and display information
+#         """
+        
+#         self.new_user.save_user()
+#         test_user = User("WhatsApp password","Bihawa Mohamed","65432*") 
+#         test_user.save_user()
+        
+#         found_user = User.find_by_details("WhatsApp password")
+        
+#         self.assertEqual(found_user.password,test_user.password)   
+        
+        
+#     def test_user_exists(self):
+#         """
+#         test to check if we can return a Boolean if we can't find the user.
+#         """
+        
+#         self.new_user.save_user()
+#         test_user = User("WhatsApp password","Bihawa Mohamed","65432*")
+#         test_user.save_user()
+        
+#         user_exists = User.user_exists("WhatsApp password")
+        
+#         self.assertTrue(user_exists)   
+        
+        
+#     def test_display_all_users(self):
+#         """
+#         returns a list of all users saved
+#         """
+        
+#         self.assertEqual(User.display_users(),User.user_list)
+        
+        
+#     def text_copy_password(self):
+#         """
+#         Test to confirm that we are copying the password from a found detail
+#         """
+        
+#         self.new_user.save_user()
+#         User.copy_password("pass1234#")
+        
+#         self.assertEqual(self.new_user.password,pyperclip.paste)    
+        
+# if __name__ == '__main__':
+#     unittest.main()
