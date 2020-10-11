d8cf8523be7bf83d41e5733d5af1db4e5202d123
BihawaM <bihawam271@gmail.com>
2020-10-11 22:18:27 +0300

    add def test save user

--- User/user_test.py
+++ User/user_test.py
@@ -27,13 +27,13 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.password,"pass1234#")
         
         
-#     def test_save_user(self):
-#         """
-#         test_save_user test case to test if the user object is saved into the user list
-#         """
+    def test_save_user(self):
+        """
+        test_save_user test case to test if the user object is saved into the user list
+        """
         
-#         self.new_user.save_user()
-#         self.assertEqual(len(User.user_list),1)     
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)     
         
         
 #     def tearDown(self):
