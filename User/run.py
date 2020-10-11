67d9686b9efb52e29d56116de94491da037fdd57
BihawaM <bihawam271@gmail.com>
2020-10-11 23:05:29 +0300

    add def main

--- User/run.py
+++ User/run.py
@@ -50,81 +50,81 @@ def display_users():
     return User.display_users()     
 
 
-# def main():
-#     print("Hey Welcome to your Password Locker.Please enter your name")
-#     user_name = input()
+def main():
+    print("Hey Welcome to your Password Locker.Please enter your name")
+    user_name = input()
     
-#     print(f"Hey {user_name}. What would you like to do?")
-#     print('/n')
+    print(f"Hey {user_name}. What would you like to do?")
+    print('/n')
     
-#     while True:
-#         print("Use these short codes: nu - create a new user, du - display users, fu - find user, ex - exit user list")
+    while True:
+        print("Use these short codes: nu - create a new user, du - display users, fu - find user, ex - exit user list")
        
-#         short_code = input().lower()
+        short_code = input().lower()
         
-#         if short_code == 'nu':
-#             print("New User")
-#             print("-"*10)
+        if short_code == 'nu':
+            print("New User")
+            print("-"*10)
             
-#             print ("Details ....")
-#             details = input()
+            print ("Details ....")
+            details = input()
             
-#             print ("login_username ....")
-#             logname = input()
+            print ("login_username ....")
+            logname = input()
             
-#             print ("password ....")
-#             password = input()
+            print ("password ....")
+            password = input()
             
-#             save_users(create_user(details,logname,password))
+            save_users(create_user(details,logname,password))
             
-#             print ('\n')
-#             print(f"New User {details} {logname} {password} created")
-#             print ('\n')
+            print ('\n')
+            print(f"New User {details} {logname} {password} created")
+            print ('\n')
             
             
-#         elif short_code == 'du':
+        elif short_code == 'du':
             
-#             if display_users():
-#                 print("Here are the informations from the user list")
-#                 print('\n')
+            if display_users():
+                print("Here are the informations from the user list")
+                print('\n')
                 
-#                 for user in display_users():
-#                     print(f"{user.details} {user.login_username} {user.password}")
+                for user in display_users():
+                    print(f"{user.details} {user.login_username} {user.password}")
                     
-#                     print('\n')
+                    print('\n')
                     
-#             else:
-#                 print('\n')
-#                 print("You don't seem to have any passwords saved yet")
-#                 print('\n')
+            else:
+                print('\n')
+                print("You don't seem to have any passwords saved yet")
+                print('\n')
                 
         
-#         elif short_code == 'fu':
-#             print("Enter the details for the password you want to find")
+        elif short_code == 'fu':
+            print("Enter the details for the password you want to find")
             
-#             search_details = input()
+            search_details = input()
             
-#             if check_existing_users(search_details):
-#                 search_user = find_user(search_details)
-#                 print(f"{search_user.login_username}")
+            if check_existing_users(search_details):
+                search_user = find_user(search_details)
+                print(f"{search_user.login_username}")
                 
-#                 print('-'* 20)
+                print('-'* 20)
                 
-#                 print(f"Details..... {search_user.details}")
+                print(f"Details..... {search_user.details}")
                 
-#                 print(f"Password..... {search_user.password}")
+                print(f"Password..... {search_user.password}")
                 
-#             else: 
-#                 print("That detail does not exist")
+            else: 
+                print("That detail does not exist")
                 
                 
-#         elif short_code == 'ex':
-#             print("Great Day ......")
-#             break
+        elif short_code == 'ex':
+            print("Great Day ......")
+            break
         
         
-#         else: 
-#             print("Please use the short codes.")
+        else: 
+            print("Please use the short codes.")
              
 # if __name__ == '__main__':
 #     main()
